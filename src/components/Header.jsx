7d4621c68bee15d0d33454675765de141aa4b42b
perzend <boss.pahan@gmail.com>
2020-02-24 11:15:03 +0300

    add project files

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+return (
+    <header className='header'>
+        <img src='https://www.logoarena.com/market/public/13251/13251_1491491150_c3.jpg' alt='Logo not found' />
+      </header>
+);
+
+}
+
+export default Header;
