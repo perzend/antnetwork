88052262d94a8464fc337547d93a62d136ecc7c0
perzend <boss.pahan@gmail.com>
2020-02-27 16:35:59 +0300

    Change structure - add Content folder. Work with Props. Move data array to index.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import Settings from "./components/Settings/Settings";
+import Profile from './components/Content/Profile/Profile';
+import Dialogs from './components/Content/Dialogs/Dialogs';
+import News from './components/Content/News/News';
+import Music from './components/Content/Music/Music';
+import Settings from "./components/Content/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
 
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-          <Route path = '/dialogs' component = {Dialogs}/>
-          <Route path = '/profile' component = {Profile}/>
-          <Route path = '/news' component = {News}/>
-          <Route path = '/music' component = {Music}/>
-          <Route path = '/settings' component = {Settings}/>
-      </div>
-    </div>
-    </BrowserRouter>
-  )
+const App = (props) => {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' render={() => <Profile posts={props.posts}/>}/>
+                    <Route path='/dialogs'
+                           render={() => (<Dialogs dialogs={props.dialogs} messages={props.messages}/>)}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/settings' render={() => <Settings/>}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,30 @@ import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
+let preparedDialogs = [
+    {id: '1', name: 'BigAnt'},
+    {id: '2', name: 'RedAnt'},
+    {id: '3', name: 'Queen'},
+    {id: '4', name: 'Vasya'},
+    {id: '5', name: 'Petya'},
+    {id: '6', name: 'Demo'},
+    {id: '7', name: 'BlackAnt'}
+]
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let preparedMessages = [
+    {id: '1', message: 'Hi Guys! WTF'},
+    {id: '2', message: 'Iam Queen'},
+    {id: '3', message: 'Relax. Take it easy'},
+]
+
+let preparedPosts = [
+    {id: '1', message: 'Hi, how are you!', likesCount: '12'},
+    {id: '2', message: 'It\'s my first post', likesCount: '4'},
+    {id: '3', message: 'This message from postsData massive', likesCount: '555'}
+]
+
+ReactDOM.render(<App dialogs={preparedDialogs} messages={preparedMessages}
+                     posts={preparedPosts}/>, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
